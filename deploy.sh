34cc8595d15bd680ef6e4fef11523afd4fc19ad7
坚果前端 <852851198@qq.com>
2021-12-18 17:14:30 +0800

    rebuilding site Sat Dec 18 17:14:30     2021

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push    origin master 
+
+
